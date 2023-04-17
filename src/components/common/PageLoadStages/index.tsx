eed6d9c74a8b7115665fbc6f807a3cb6fba5914b
Yaroslav <yr.pryhoda@gmail.com>
2023-04-17 17:21:12 +0300

    Refactor to Clean code principes

--- src/components/common/PageLoadStages/index.tsx
+++ src/components/common/PageLoadStages/index.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import {ErrorHandler} from '../ErrorHandler';
-import {PageLayout} from '../PageLayout';
-import {Spinner} from '../Spinner';
+import { ErrorHandler } from "../ErrorHandler";
+import { PageLayout } from "../PageLayout";
+import { Spinner } from "../Spinner";
 
 interface IProps {
-    loading: boolean;
-    error: string | null;
+  loading: boolean;
+  error: string | null;
 }
 
-export const PageLoadStages = ({loading, error}: IProps) => {
-    return <PageLayout>
-        {
-            loading
-                ? <Spinner/>
-                : error
-                    ? <ErrorHandler errMessage={error}/>
-                    : <h6>There is nothing found here</h6>
-        }
-    </PageLayout>;
+export const PageLoadStages = ({ loading, error }: IProps) => {
+  if (loading) {
+    return (
+      <PageLayout>
+        <Spinner />
+      </PageLayout>
+    );
+  }
+
+  if (error) {
+    return (
+      <PageLayout>
+        <ErrorHandler errMessage={error} />
+      </PageLayout>
+    );
+  }
+	
+  return (
+    <PageLayout>
+      <h6>There is nothing found here</h6>
+    </PageLayout>
+  );
 };
