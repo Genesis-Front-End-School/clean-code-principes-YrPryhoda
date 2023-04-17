30d433dfb70ed2fe44d483894e8723b549068bd0
Yaroslav <yr.pryhoda@gmail.com>
2023-04-17 17:17:28 +0300

    Fix footer render

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,8 +1,9 @@
 import {Grid} from '@mui/material';
 import styles from './styles.module.scss';
+import { useMemo } from 'react';
 
 export const Footer = () => {
-    const currentYear = new Date().getFullYear();
+    const currentYear = useMemo(() => new Date().getFullYear(), []);
 
     return <Grid container className={styles.footer}>
         <p className={styles.footer__title}>Created for Genesis Academy in {currentYear}</p>
